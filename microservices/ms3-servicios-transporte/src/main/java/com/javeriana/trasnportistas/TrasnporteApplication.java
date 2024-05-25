dd01287be61e32bbaee32d51f930f8aac78bdfd4
Camilo M <105135927+Kahsma@users.noreply.github.com>
2024-05-25 17:35:23 -0500

    Add ms-3 Transportes

---
+++ microservices/ms3-servicios-transporte/src/main/java/com/javeriana/trasnportistas/TrasnporteApplication.java
@@ -0,0 +1,13 @@
+package com.javeriana.trasnportistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasnporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasnporteApplication.class, args);
+	}
+
+}
