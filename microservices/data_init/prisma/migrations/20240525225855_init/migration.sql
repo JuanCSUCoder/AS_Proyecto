085c2fc93d92daf761475fe1d64cb5a065f27e02
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-25 18:00:35 -0500

    Feat: Define el init.sql y el modelo con prisma

--- microservices/data_init/prisma/migrations/20240525225855_init/migration.sql
+++ microservices/data_init/prisma/migrations/20240525225855_init/migration.sql
@@ -2,7 +2,8 @@
 CREATE TABLE "Product" (
     "id" STRING NOT NULL,
     "name" STRING NOT NULL,
-    "desc" STRING NOT NULL,
+    "descr" STRING NOT NULL,
+    "imageURL" STRING NOT NULL,
     "price" FLOAT8 NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
@@ -48,19 +49,6 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "Address" (
-    "id" STRING NOT NULL,
-    "userId" STRING NOT NULL,
-    "street" STRING NOT NULL,
-    "city" STRING NOT NULL,
-    "state" STRING NOT NULL,
-    "country" STRING NOT NULL,
-    "zip" STRING NOT NULL,
-
-    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Review" (
     "id" STRING NOT NULL,
     "score" INT4 NOT NULL,
@@ -75,9 +63,6 @@ CREATE UNIQUE INDEX "Inventory_productId_key" ON "Inventory"("productId");
 -- CreateIndex
 CREATE UNIQUE INDEX "User_userPod_key" ON "User"("userPod");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Address_userId_key" ON "Address"("userId");
-
 -- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -89,9 +74,6 @@ ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("or
 
 -- AddForeignKey
 ALTER TABLE "Inventory" ADD CONSTRAINT "Inventory_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Review" ADD CONSTRAINT "Review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
