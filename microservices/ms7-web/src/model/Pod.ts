276e52461ee3cf0692af609b5fb8f0a92f4778a4
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-26 18:51:29 -0500

    Feat: Agrega al modelo una estructura tentativa de Pod

---
+++ microservices/ms7-web/src/model/Pod.ts
@@ -0,0 +1,37 @@
+export interface Pod {
+  name?: string,
+  email?: string,
+  location?: {
+    description?: {
+      address?: string,
+      city?: string,
+      countryCode?: string
+    },
+    coordinates?: {
+      lat?: number,
+      lng?: number
+    }
+  },
+  private: {
+    cardNumber?: string,
+    ccv?: number
+  } 
+}
+
+/*
+{
+  "name": "Juan Camilo Sánchez Urrego",
+  "email": "juancsucoder@gmail.com",
+  "location": {
+    "description": {
+      "address": "Cll 123 #12-12",
+      "city": "Bogotá D.C.", // Nombre de la ciudad
+      "countryCode": "COL" // ISO 3 digit code
+    },
+    "coordinates": {
+      "lat": 42.251643,
+      "lng": 52.024531
+    }
+  }
+}
+*/
