bdd9b848177599a1a5384bfbf2572ed91719071a
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-25 19:40:11 -0500

    Feat: Configura la persistencia

--- microservices/ms12_calificaciones/src/test/java/co/edu/javeriana/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package co.edu.javeriana;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
