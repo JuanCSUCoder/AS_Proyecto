da6e3296fe098b391aa1038676b7de9e6f25adb6
ALEYI17 <aleelaleyi@gmail.com>
2024-05-25 23:06:34 -0500

    Terine el microservicio de pedidos

--- microservices/mas-10-gestion-pedidos/src/main/java/com/pedidos/model/User.java
+++ microservices/mas-10-gestion-pedidos/src/main/java/com/pedidos/model/User.java
@@ -1,14 +1,13 @@
-package co.edu.javeriana.model;
+package  com.pedidos.model;
 
 import java.util.List;
 
+import jakarta.json.bind.annotation.JsonbTransient;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,23 +15,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "\"Order\"")
+@Table(name = "\"User\"")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Order {
+public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   String id;
 
-  String status;
-  double total;
-
-  @OneToOne
-  @JoinColumn(name = "userId", referencedColumnName = "id")
-  User user;
-
+  String userPod;
+  String providerUrl;
+  
+  @JsonbTransient
   @OneToMany
-  List<OrderItem> items;
+  List<Order> orders;
 }
