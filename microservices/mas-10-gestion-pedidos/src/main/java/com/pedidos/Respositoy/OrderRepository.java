e9735d2717d667d7d59f71131cbf6a3a76143798
ALEYI17 <aleelaleyi@gmail.com>
2024-05-25 20:04:49 -0500

    cambie para el n uevo modelo

--- microservices/mas-10-gestion-pedidos/src/main/java/com/pedidos/Respositoy/OrderRepository.java
+++ microservices/mas-10-gestion-pedidos/src/main/java/com/pedidos/Respositoy/OrderRepository.java
@@ -9,28 +9,28 @@ import java.util.List;
 
 import java.util.stream.Collectors;
 
+import com.pedidos.modelo.OrderEntity;
+import com.pedidos.modelo.OrderItemEntity;
+import com.pedidos.modelo.ProductEntity;
+import com.pedidos.modelo.UserEntity;
 
-import com.pedidos.modelo.Order;
-import com.pedidos.modelo.OrderItem;
-import com.pedidos.modelo.Product;
-import com.pedidos.modelo.User;
 
 @ApplicationScoped
 public class OrderRepository {
 
-    private List<Order> orders = new ArrayList<>();
-    private List<OrderItem> orderItems = new ArrayList<>();
-    private List<Product> products = new ArrayList<>();
-    private List<User> users = new ArrayList<>();
+    private List<OrderEntity> orders = new ArrayList<>();
+    private List<OrderItemEntity> orderItems = new ArrayList<>();
+    private List<ProductEntity> products = new ArrayList<>();
+    private List<UserEntity> users = new ArrayList<>();
 
     public OrderRepository() {
         // Datos quemados para usuarios
-        User user1 = new User();
+        UserEntity user1 = new UserEntity();
         user1.setId("user1");
         user1.setUserPod("userPod1");
         user1.setProviderUrl("providerUrl1");
 
-        User user2 = new User();
+        UserEntity user2 = new UserEntity();
         user2.setId("user2");
         user2.setUserPod("userPod2");
         user2.setProviderUrl("providerUrl2");
@@ -39,32 +39,32 @@ public class OrderRepository {
         users.add(user2);
 
         // Datos quemados para productos
-        Product product1 = new Product();
+        ProductEntity product1 = new ProductEntity();
         product1.setId("product1");
         product1.setName("Product 1");
-        product1.setDesc("Description for product 1");
+        product1.setDescr("Description for product 1");
         product1.setPrice(10.0f);
-        product1.setStock(100);
+        
 
-        Product product2 = new Product();
+        ProductEntity product2 = new ProductEntity();
         product2.setId("product2");
         product2.setName("Product 2");
-        product2.setDesc("Description for product 2");
+        product2.setDescr("Description for product 2");
         product2.setPrice(20.0f);
-        product2.setStock(200);
+        
 
         products.add(product1);
         products.add(product2);
 
         // Datos quemados para pedidos
-        OrderItem orderItem1 = new OrderItem();
+        OrderItemEntity orderItem1 = new OrderItemEntity();
         orderItem1.setId("orderItem1");
         orderItem1.setOrderId("order1");
         orderItem1.setProductId("product1");
         orderItem1.setQuantity(2);
         
 
-        OrderItem orderItem2 = new OrderItem();
+        OrderItemEntity orderItem2 = new OrderItemEntity();
         orderItem2.setId("orderItem2");
         orderItem2.setOrderId("order1");
         orderItem2.setProductId("product2");
@@ -74,8 +74,8 @@ public class OrderRepository {
         orderItems.add(orderItem1);
         orderItems.add(orderItem2);
 
-        Order order1 = new Order();
-        order1.setId(1);
+        OrderEntity order1 = new OrderEntity();
+        order1.setId("1");
         order1.setUserId("user1");
         order1.setStatus("Pending");
         order1.setTotal(40.0f);
@@ -83,14 +83,14 @@ public class OrderRepository {
         orders.add(order1);
     }
 
-    public Order save(Order order) {
+    public OrderEntity save(OrderEntity order) {
         orders.add(order);
         return order;
     }
 
-    public Order update(Order order, Integer orderId){
+    public OrderEntity update(OrderEntity order, String orderId){
         System.out.println(orderId);
-        Order orderchange = findById(orderId);
+        OrderEntity orderchange = findById(orderId);
         if(orderchange != null){
             for (int i = 0 ; i < orders.size();i++){
                 if (orders.get(i).getId().equals(orderId)){
@@ -104,7 +104,7 @@ public class OrderRepository {
         return null;
     }
 
-    public Order findById(Integer id) {
+    public OrderEntity findById(String id) {
         
         for (int i = 0 ; i < orders.size();i++){
             
@@ -117,16 +117,16 @@ public class OrderRepository {
         return null;
     }
 
-    public List<Order> findByUserId(String userId) {
+    public List<OrderEntity> findByUserId(String userId) {
         return orders.stream().filter(order -> order.getUserId().equals(userId)).collect(Collectors.toList());
     }
 
-    public List<Order> getAllOrders(){
+    public List<OrderEntity> getAllOrders(){
         return orders;
     }
 
-    public List<Order> getOrdenClient(String clientID) {
-        List<Order> clientOrders = new ArrayList<>();
+    public List<OrderEntity> getOrdenClient(String clientID) {
+        List<OrderEntity> clientOrders = new ArrayList<>();
         for (int i = 0 ; i < orders.size();i++){
             String user = orders.get(i).getUserId();
             if ( user.equals(clientID)){
