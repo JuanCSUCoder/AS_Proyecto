5e88b3c140c3e1377e387b3be5bfcd972f376990
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-26 18:44:02 -0500

    Feat: Modelo de order

--- microservices/ms7-web/src/model/Order.ts
+++ microservices/ms7-web/src/model/Order.ts
@@ -1,3 +1,10 @@
+import { OrderItem } from "./OrderItem";
+import { User } from "./User";
+
 export interface Order {
-  id: String
+  id: string,
+  status: string,
+  total: number,
+  user: User,
+  items: OrderItem[]
 }
