cc61a9322b6c679ca85739293e2e9a55e4b9c56a
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-26 18:48:27 -0500

    Feat: Termina el modelo

--- microservices/ms7-web/src/model/OrderItem.ts
+++ microservices/ms7-web/src/model/OrderItem.ts
@@ -1,3 +1,9 @@
+import { Order } from "./Order";
+import { Product } from "./Product";
+
 export interface OrderItem {
-  id: String;
+  id: string,
+  quantity: number,
+  order: Order,
+  product: Product
 }
