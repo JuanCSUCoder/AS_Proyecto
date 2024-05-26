7921562f78764048decf19938f3db71b4a6afcee
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-25 20:12:50 -0500

    Feat: Copia el modelo al proyecto de quarkus

---
+++ microservices/ms12_calificaciones/src/main/java/co/edu/javeriana/model/Product.java
@@ -0,0 +1,41 @@
+package co.edu.javeriana;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  String id;
+
+  String name;
+  String descr;
+
+  String imageURL;
+  
+  double price;
+
+  @OneToOne
+  Inventory inventory;
+
+  @OneToMany
+  List<Review> reviews;
+
+  @OneToMany
+  List<OrderItem> orderItems;
+}
