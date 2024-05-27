dc65078f12f2477ce275b02c30b5cae0f8e951c0
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 16:01:26 -0500

    Feat: Implementa context en Cartitem eliminar

--- microservices/ms7-web/src/components/cart/CartItem.tsx
+++ microservices/ms7-web/src/components/cart/CartItem.tsx
@@ -1,10 +1,13 @@
 import { OrderItem } from "@/model/OrderItem";
+import { CartContext, CartProvider } from "@/providers/CartProvider";
 import { Delete } from "@mui/icons-material";
 import { Button } from "@mui/joy";
+import { useContext } from "react";
 
 export function CartItem({ orderItem }: {
   orderItem: OrderItem
 }) {
+  const ctx = useContext(CartContext);
   const { product, quantity } = orderItem;
 
   return (
@@ -13,7 +16,7 @@ export function CartItem({ orderItem }: {
       <td>{quantity}x</td>
       <td>$ {product?.price}</td>
       <td>
-        <Button color="danger">
+        <Button color="danger" onClick={() => product && ctx?.removeItem(product)}>
           <Delete />
           <span className="hidden lg:inline-block">Eliminar</span>
         </Button>
