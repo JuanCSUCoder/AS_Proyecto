56bcc5aeff39b651c49657fdebcf772e06c22442
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-26 23:36:25 -0500

    Fix: Responsive

--- microservices/ms7-web/src/app/order/user/verify/page.tsx
+++ microservices/ms7-web/src/app/order/user/verify/page.tsx
@@ -15,28 +15,33 @@ export default function VerifyDataPage() {
     <DefaultContainer>
       <h2>Verifica tu información</h2>
       <FormBox onSubmit={onSubmit}>
-        <h3>Información Personal</h3>
-        <FormField required name="name">
-          Nombre:
-        </FormField>
-        <FormField type="email" required name="email">
-          Email:
-        </FormField>
+        <div className="flex flex-col md:flex-row gap-9 w-full">
+          <div className="flex flex-col w-full md:w-1/2">
+            <h3>Información Personal</h3>
+            <FormField required name="name">
+              Nombre:
+            </FormField>
+            <FormField type="email" required name="email">
+              Email:
+            </FormField>
+          </div>
 
-        <h3>Información de Entrega</h3>
-        <FormField required name="address">
-          Dirección:
-        </FormField>
-        <FormField required name="city">
-          Ciudad:
-        </FormField>
-
-        <FormField required name="lat">
-          Latitud:
-        </FormField>
-        <FormField required name="lng">
-          Longitud:
-        </FormField>
+          <div className="flex flex-col w-full md:w-1/2">
+            <h3>Información de Entrega</h3>
+            <FormField required name="address">
+              Dirección:
+            </FormField>
+            <FormField required name="city">
+              Ciudad:
+            </FormField>
+            <FormField required name="lat">
+              Latitud:
+            </FormField>
+            <FormField required name="lng">
+              Longitud:
+            </FormField>
+          </div>
+        </div>
 
         <h3>Información de Pago</h3>
         <FormField required type="number" name="cardNumber">
