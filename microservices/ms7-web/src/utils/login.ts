a4d69485d28594ce6532b8eb9a3389dc22b4a837
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 18:22:10 -0500

    Feat: Carga las URLs de variables de entorno

--- microservices/ms7-web/src/utils/login.ts
+++ microservices/ms7-web/src/utils/login.ts
@@ -1,10 +1,10 @@
 const test = {
-  api: "http://localhost:5006",
-  callback: "http://localhost:3000",
+  api: process.env.MS5_URL,
+  callback: process.env.SELF_URL,
 };
 
 export function loginCallback(idp: string, callback: string) {
   window.location.href = `${test.api}/login/?idp=${encodeURIComponent(
     idp
-  )}&&callback=${encodeURIComponent(test.callback + callback)}`;
+  )}&callback=${encodeURIComponent(test.callback + callback)}`;
 }
