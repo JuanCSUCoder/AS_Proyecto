814007a7f203702d5ac31601c4821fba0a1eac9b
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 16:35:55 -0500

    Fixes: Reload State Issues

--- microservices/ms7-web/src/providers/CartProvider.tsx
+++ microservices/ms7-web/src/providers/CartProvider.tsx
@@ -28,41 +28,49 @@ export function CartProvider({ children }: { children: ReactNode }) {
   });
 
   const addItem: ItemHandler = (item) => {
+    console.log("Adding item")
     setCart((prevCart) => {
-      const existingItem = prevCart.cart.items?.findLast((i) => i.product?.id == item.id);
+      const newCart: CartState = JSON.parse(JSON.stringify(prevCart));
+      const existingItem = newCart.cart.items?.findLast((i) => i.product?.id == item.id);
 
       if (existingItem && existingItem.quantity) {
+        console.log("FOund")
         existingItem.quantity += 1;
       } else {
-        prevCart.cart.items?.push({
-          order: prevCart.cart,
+        console.log("Not found creating...");
+        newCart.cart.items?.push({
+          order: newCart.cart,
           product: item,
           quantity: 1,
         });
       }
-      return {...prevCart};
+      return newCart;
     })
     return true;
   };
   
   const removeItem: ItemHandler = (item) => {
     setCart((prevCart) => {
-      const existingItem = prevCart.cart.items?.findLast(
+      const newCart: CartState = JSON.parse(JSON.stringify(prevCart));
+      const existingItem = newCart.cart.items?.findLast(
         (i) => i.product?.id == item.id
       );
 
+      console.log("Trying to remove 1 of " + existingItem);
+
       if (existingItem && existingItem.quantity) {
         if (existingItem.quantity <= 1) {
-          const index = prevCart.cart.items?.indexOf(existingItem);
+          const index = newCart.cart.items?.indexOf(existingItem);
+          console.log("Found for deleition at " + index);
           if (index && index > -1) {
-            prevCart.cart.items?.splice(index, 1);
+            newCart.cart.items?.splice(index, 1);
           }
         } else {
           existingItem.quantity -= 1;
         }
       }
 
-      return {...prevCart};
+      return newCart;
     });
     return true;
   }
