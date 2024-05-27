01d437eceec78ea981ccb3b293ba99a3e586343c
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 15:55:04 -0500

    Feat: Conecta el carrito con el provider

--- microservices/ms7-web/src/app/cart/page.tsx
+++ microservices/ms7-web/src/app/cart/page.tsx
@@ -1,49 +1,59 @@
+"use client"
+
 import { CartList } from "@/components/cart/CartList";
 import { PriceTag } from "@/components/products/PriceTag";
 import { BtnGroup } from "@/components/utils/BtnGroup";
 import { DefaultContainer } from "@/components/utils/DefaultContainer";
 import { MainButton } from "@/components/utils/MainButton";
 import { Order } from "@/model/Order";
+import { CartContext } from "@/providers/CartProvider";
 import { Button } from "@mui/joy";
 import Link from "next/link";
+import { useContext } from "react";
 
 export default function CartPage() {
-  const order: Order = {
-    id: "485629-458720-246524-246246",
-    status: "PENDING",
-    total: 148.08,
-    items: [
-      {
-        id: "1",
-        quantity: 3,
-        product: {
-          id: "1",
-          descr: "Descripcion producto",
-          name: "Cocacola",
-          price: 12.34,
-        },
-      },
-      {
-        id: "2",
-        quantity: 1,
-        product: {
-          id: "2",
-          descr: "Descripcion producto 2",
-          name: "Arduino Uno",
-          price: 12.34,
-        },
-      },
-      {
-        id: "3",
-        quantity: 8,
-        product: {
-          id: "3",
-          descr: "Descripcion producto 3",
-          name: "Avena",
-          price: 12.34,
-        },
-      },
-    ],
+  // const order: Order = {
+  //   id: "485629-458720-246524-246246",
+  //   status: "PENDING",
+  //   total: 148.08,
+  //   items: [
+  //     {
+  //       id: "1",
+  //       quantity: 3,
+  //       product: {
+  //         id: "1",
+  //         descr: "Descripcion producto",
+  //         name: "Cocacola",
+  //         price: 12.34,
+  //       },
+  //     },
+  //     {
+  //       id: "2",
+  //       quantity: 1,
+  //       product: {
+  //         id: "2",
+  //         descr: "Descripcion producto 2",
+  //         name: "Arduino Uno",
+  //         price: 12.34,
+  //       },
+  //     },
+  //     {
+  //       id: "3",
+  //       quantity: 8,
+  //       product: {
+  //         id: "3",
+  //         descr: "Descripcion producto 3",
+  //         name: "Avena",
+  //         price: 12.34,
+  //       },
+  //     },
+  //   ],
+  // };
+
+  const context = useContext(CartContext);
+  const order = context ? context.cartState.cart : {
+    total: 0.0,
+    items: []
   };
 
   return (
