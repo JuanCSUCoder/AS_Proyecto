8f3b760dad263022b42cec838d47a2d1273b1fee
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 15:58:54 -0500

    Feat: Integrate provider into productcard

--- microservices/ms7-web/src/components/products/ProductCard.tsx
+++ microservices/ms7-web/src/components/products/ProductCard.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Product } from "@/model/Product";
 import { AspectRatio, Button, Card, CardOverflow } from "@mui/joy";
 import { PriceTag } from "./PriceTag";
@@ -5,10 +7,14 @@ import { Add, Delete, Info } from "@mui/icons-material";
 import Image from "next/image";
 import { BtnGroup } from "../utils/BtnGroup";
 import Link from "next/link";
+import { useContext } from "react";
+import { CartContext } from "@/providers/CartProvider";
 
 export function ProductCard({ product }: {
   product: Product
 }) {
+  const ctx = useContext(CartContext);
+
   return (
     <Card orientation="vertical" size="lg" variant="soft" sx={{ width: 320 }}>
       <h3 className="my-1">{product.name}</h3>
@@ -31,8 +37,8 @@ export function ProductCard({ product }: {
       <p className="my-1">{product.descr}</p>
 
       <BtnGroup>
-        <Button startDecorator={<Add />}>Agregar</Button>
-        <Button startDecorator={<Delete />} color="danger">
+        <Button startDecorator={<Add />} onClick={() => ctx?.addItem(product)}>Agregar</Button>
+        <Button startDecorator={<Delete />} color="danger" onClick={() => ctx?.removeItem(product)}>
           Eliminar
         </Button>
         <Link href={`/product/${product.id}`}>
