9e447dbb3f594f8c720031c1ad55e48e9476e033
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-28 01:11:43 -0500

    FEat: Implementa API de Productos

--- microservices/ms7-web/src/components/products/Products.tsx
+++ microservices/ms7-web/src/components/products/Products.tsx
@@ -1,91 +1,91 @@
 import { Product } from "@/model/Product"
 import { ProductCard } from "./ProductCard";
 
-export function Products() {
-  const products: Product[] = [
-    {
-      name: "Producto 1",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-      imageURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Arduino_ftdi_chip-1.jpg/1200px-Arduino_ftdi_chip-1.jpg",
-    },
-    {
-      name: "Producto 2",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 1",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 2",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 1",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 2",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 1",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 2",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-    {
-      name: "Producto 3",
-      descr: "Descrripcion del producto 1",
-      price: 1120.0,
-    },
-  ];
+export function Products({products}: {products: Product[]}) {
+  // const products: Product[] = [
+  //   {
+  //     name: "Producto 1",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //     imageURL:
+  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Arduino_ftdi_chip-1.jpg/1200px-Arduino_ftdi_chip-1.jpg",
+  //   },
+  //   {
+  //     name: "Producto 2",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 1",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 2",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 1",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 2",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 1",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 2",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  //   {
+  //     name: "Producto 3",
+  //     descr: "Descrripcion del producto 1",
+  //     price: 1120.0,
+  //   },
+  // ];
 
   let idcount = 0;
   products.forEach(product => {
