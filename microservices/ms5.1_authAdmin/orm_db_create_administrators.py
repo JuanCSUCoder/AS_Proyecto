265b686ab0f2b6417d35ac385263f8115097a605
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-05-27 19:39:46 -0500

    Added CORS to MS15_estadisticas and ms5.1_auth init

---
+++ microservices/ms5.1_authAdmin/orm_db_create_administrators.py
@@ -0,0 +1,47 @@
+import psycopg2
+import time
+
+# Información de conexión
+conn = psycopg2.connect(
+    dbname='defaultdb',
+    user='root',
+    password='',
+    host='localhost', # Cambiar a cockroachdb
+    port='26257'
+)
+
+# Esperar 5 segundos
+time.sleep(5)
+
+# Crear la tabla administrators
+def create_tables():
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS administrators (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR UNIQUE NOT NULL,
+                password VARCHAR NOT NULL
+            );
+        """)
+        conn.commit()
+
+# Insertar administrador
+def insert_admin():
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO administrators (id, username, password)
+            VALUES (1, 'root', '1234')
+            ON CONFLICT DO NOTHING;
+        """)
+        conn.commit()
+
+# Crear la tabla
+create_tables()
+print("Table 'administrators' created")
+
+# Insertar administrador
+insert_admin()
+print("Admin 'root' inserted")
+
+# Cerrar la conexión
+conn.close()
