1bb1274fd7fb17c1fde71322b7ce15e1048a5088
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 21:26:38 -0500

    Feat: Implementa DBUser

--- microservices/ms7-web/src/app/hooks/useDBUser.ts
+++ microservices/ms7-web/src/app/hooks/useDBUser.ts
@@ -6,7 +6,20 @@ export function useDBUser(webId?:string) {
 
   useEffect(() => {
     fetch("http://localhost:5007/gestionusuarios/api/users/search?userPod=" + webId)
-      .then(res => res.json())
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          fetch("http://localhost:5007/gestionusuarios/api/users", {
+            method: "POST",
+            body: JSON.stringify({
+              userPod: webId,
+              providerUrl: webId,
+            }),
+          }).then(res => res.json())
+            .then(res => setUser(res));
+        }
+      })
       .then(res => setUser(res));
   }, [webId])
 
