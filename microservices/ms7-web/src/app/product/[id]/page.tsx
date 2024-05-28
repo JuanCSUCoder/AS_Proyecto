d48326d99df26574894fae40e94b87c15b0ec5eb
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-28 08:17:22 -0500

    Fix: CORS

--- microservices/ms7-web/src/app/product/[id]/page.tsx
+++ microservices/ms7-web/src/app/product/[id]/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import { FormBox } from "@/components/order/login/FormBox";
+import { FormField } from "@/components/order/login/FormField";
 import { PriceTag } from "@/components/products/PriceTag";
 import { ProductBtns } from "@/components/products/ProductBtns";
 import { BtnGroup } from "@/components/utils/BtnGroup";
 import { DefaultContainer } from "@/components/utils/DefaultContainer";
+import { MainButton } from "@/components/utils/MainButton";
 import { Product } from "@/model/Product";
+import { Review } from "@/model/Review";
 import { Add, Delete } from "@mui/icons-material";
 import { Button } from "@mui/joy";
 import Image from "next/image";
@@ -24,11 +28,16 @@ export default function ProductDetailsPage({ params }: {
   // };
 
   const [product, setProd] = useState<Product | undefined>(undefined);
+  const [scores, setScores] = useState<Review[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:5014/products/" + params.id)
       .then(res => res.json())
       .then(res => setProd(res));
+    
+    fetch("http://localhost:5013/scores?prodId=" + params.id)
+      .then(res => res.json())
+      .then(res => setScores(res));
   }, [params.id])
 
   if (!product) return (<p>Cargando ...</p>);
@@ -54,6 +63,16 @@ export default function ProductDetailsPage({ params }: {
         <div className="flex w-full flex-col items-end md:mt-20">
           <PriceTag>{product.price}</PriceTag>
           <ProductBtns product={product} />
+          <FormBox onSubmit={(e) => {
+            e.preventDefault();
+          }}>
+            <FormField name="comment">Comentario</FormField>
+            <FormField name="score" type="number">Puntuación 0 a 5:</FormField>
+            <MainButton type="submit">Enviar Comentario</MainButton>
+          </FormBox>
+          <div>
+
+          </div>
         </div>
       </div>
     </DefaultContainer>
