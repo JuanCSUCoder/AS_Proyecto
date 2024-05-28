fbf8df8cad84889f0364f0539ba9bcd9e7fb73cc
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 23:15:33 -0500

    Feat: Implementa recomendaciones de productos

--- microservices/ms7-web/src/app/page.tsx
+++ microservices/ms7-web/src/app/page.tsx
@@ -1,26 +1,32 @@
 "use client"
 
-import { NavBar } from "@/components/NavBar";
 import { Products } from "@/components/products/Products";
-import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useDBUser } from "./hooks/useDBUser";
+import { usePodInfo } from "./hooks/usePodInfo";
 
 export default function Home() {
+  const podInfo = usePodInfo();
+  const dbUser = useDBUser(podInfo?.webId);
+  const [suggestion, setSuggestion] = useState("");
+
   useEffect(() => {
-    try {
-      fetch("http://localhost:5004/recomendaciones_productos", {
-        method: "POST",
-        body: JSON.stringify({
-          user_id: ""
-        })
-      });
-    } catch (error) {
-      
-    }
-  }, []);
+    fetch("http://localhost:5004/recomendaciones_productos", {
+      method: "POST",
+      body: JSON.stringify({
+        user_id: dbUser?.id
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => res.text())
+      .then(res => setSuggestion(res))
+      .catch(err => console.error(err));
+  }, [dbUser?.id]);
 
   return (
     <main className="flex w-full flex-col items-center justify-start">
+      <p>{ suggestion }</p>
       <Products />
     </main>
   );
