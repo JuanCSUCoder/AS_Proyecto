298a83908ebcf23d8e39466d4c394512d101e974
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 21:36:31 -0500

    Feat: Implementa DBUser

--- microservices/ms7-web/src/app/hooks/useDBUser.ts
+++ microservices/ms7-web/src/app/hooks/useDBUser.ts
@@ -5,22 +5,36 @@ export function useDBUser(webId?:string) {
   const [user, setUser] = useState<User | undefined>(undefined);
 
   useEffect(() => {
-    fetch("http://localhost:5007/gestionusuarios/api/users/search?userPod=" + webId)
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          fetch("http://localhost:5007/gestionusuarios/api/users", {
-            method: "POST",
-            body: JSON.stringify({
-              userPod: webId,
-              providerUrl: webId,
-            }),
-          }).then(res => res.json())
-            .then(res => setUser(res));
-        }
-      })
-      .then(res => setUser(res));
+    const createUser = () => {
+      if (webId) {
+        fetch("http://localhost:5007/gestionusuarios/api/users", {
+          method: "POST",
+          body: JSON.stringify({
+            userPod: webId,
+            providerUrl: webId,
+          }),
+        })
+          .then((res) => res.json())
+          .then((res) => setUser(res));
+      }
+    }
+
+    try {
+      fetch(
+        "http://localhost:5007/gestionusuarios/api/users/search?userPod=" +
+          webId
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            createUser();
+          }
+        })
+        .then((res) => setUser(res));
+    } catch (error) {
+      createUser();
+    }
   }, [webId])
 
   return user;
