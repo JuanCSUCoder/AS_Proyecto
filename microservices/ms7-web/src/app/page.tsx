97683006a66a83b10149b66e98d3b39e34e8a426
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-27 20:37:34 -0500

    Feat: Base para integrar recomendaciones de IA

--- microservices/ms7-web/src/app/page.tsx
+++ microservices/ms7-web/src/app/page.tsx
@@ -1,8 +1,22 @@
 import { NavBar } from "@/components/NavBar";
 import { Products } from "@/components/products/Products";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    try {
+      fetch("http://localhost:5004/recomendaciones_productos", {
+        method: "POST",
+        body: JSON.stringify({
+          user_id: ""
+        })
+      });
+    } catch (error) {
+      
+    }
+  }, []);
+
   return (
     <main className="flex w-full flex-col items-center justify-start">
       <Products />
